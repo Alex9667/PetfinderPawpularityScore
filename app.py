75cde0b3576e7ea7373337b8de355c0c96a42061
Alex9667 <blablabla2013@live.dk>
2024-03-04 14:23:37 +0100

    regression

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 import re
-from datetime import datetime
-
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
 from flask import Flask
 
+from sklearn.linear_model import LinearRegression
+
 app = Flask(__name__)
+
+data_path = r'Data\train.csv'
+temp_data = pd.read_csv(data_path, index_col=['Id'])
+
+data = pd.DataFrame(temp_data)
+
+X = data.drop(columns=['Pawpularity']).values
+y = data['Pawpularity'].values
+
+reg = LinearRegression()
+reg.fit(X,y)
+
+print(reg.score(X,y))
+
+print(reg.predict([[0,0,0,0,0,0,0,0,0,0,0,0]]))
 
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return 'Godav!'
