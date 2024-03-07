0281408c36247ffca4df4190228a7e074f05ca76
Alex9667 <blablabla2013@live.dk>
2024-03-07 13:53:39 +0100

    Added image and prediction pages with functionality. Still need to add predictions with the images

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-import re
 import pandas as pd
-import seaborn as sn
-import matplotlib.pyplot as plt
+
+from flask import Flask, render_template, send_from_directory
+from flask_bootstrap import Bootstrap5
+
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import SubmitField, BooleanField, StringField
 
 from flask import Flask
 
 from sklearn.linear_model import LinearRegression
 
+
 app = Flask(__name__)
+app.secret_key = 'tO$&!|0wkamvVia0?n$NqIRVWOG'
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
+
 
 data_path = r'Data\train.csv'
 temp_data = pd.read_csv(data_path, index_col=['Id'])
@@ -20,11 +28,66 @@ y = data['Pawpularity'].values
 reg = LinearRegression()
 reg.fit(X,y)
 
-print(reg.score(X,y))
 
-print(reg.predict([[0,0,0,0,0,0,0,0,0,0,0,0]]))
+def predictScore(Subject_focus, Eyes, Face, Near, Action, Accessory, Group, Collage, Human, Occlusion, Info, Blur):
+    return str(reg.predict([[Subject_focus, Eyes, Face, Near, Action, Accessory, Group, Collage, Human, Occlusion, Info, Blur]]))
 
 
-@app.route("/")
-def home():
-    return 'Godav!'
+class PredictForm(FlaskForm):
+    subject_focus = BooleanField('Subject_focus')
+    eyes = BooleanField('Eyes')
+    face = BooleanField('Face')
+    near = BooleanField('Near')
+    action = BooleanField('Action')
+    accessory = BooleanField('Accessory')
+    group = BooleanField('Group')
+    collage = BooleanField('Collage')
+    human = BooleanField('Human')
+    occlusion = BooleanField('Occlusion')
+    info = BooleanField('Info')
+    blur = BooleanField('Blue')
+
+    submit = SubmitField('Submit')
+
+class ImageScoreForm(FlaskForm):
+    id = StringField('Id')
+    score = 0
+        
+    submit = SubmitField('Submit')
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template('header.html')
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    form = PredictForm()
+
+    if form.validate_on_submit():
+        subject_focus = form.subject_focus.data
+        eyes = form.eyes.data
+        face = form.face.data
+        near = form.near.data
+        action = form.action.data
+        accessory = form.accessory.data
+        group = form.group.data
+        collage = form.collage.data
+        human = form.human.data
+        occlusion = form.occlusion.data
+        info = form.info.data
+        blur = form.blur.data
+
+        return predictScore(int(subject_focus), int(eyes), int(face), int(near), int(action), int(accessory), int(group), int(collage), int(human), int(occlusion), int(info), int(blur))        
+
+    return render_template('predictScore.html', form=form)
+
+@app.route("/imageScore", methods=['GET', 'POST'])
+def imageScore():
+    form = ImageScoreForm()
+    if form.validate_on_submit():
+        id = form.id.data
+
+        return send_from_directory('Data/train', id + '.jpg')
+    
+    return render_template('imageScore.html', form=form)
