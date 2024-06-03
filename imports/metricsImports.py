3d3b9d804603e77a173472cccf5e757a7fc9152d
Christina-stokbak <94115578+Christina-stokbak@users.noreply.github.com>
2024-06-03 13:23:22 +0200

    Fixed unsupervised methods (clustering)
    Added PCA before fit

--- imports/metricsImports.py
+++ imports/metricsImports.py
@@ -1,3 +1,4 @@
+import sklearn.metrics as metrics
 from sklearn.metrics import roc_curve, auc, confusion_matrix, f1_score, recall_score, precision_score, accuracy_score
 
-__all__ = ['roc_curve', 'auc', 'confusion_matrix', 'f1_score', 'recall_score', 'precision_score', 'accuracy_score']
+__all__ = ['metrics', 'roc_curve', 'auc', 'confusion_matrix', 'f1_score', 'recall_score', 'precision_score', 'accuracy_score']
